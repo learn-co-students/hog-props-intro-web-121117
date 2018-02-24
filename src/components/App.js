945869de6a1f5372a895f7fcdc48c3d51392e8a2
github username <github email address>
2018-02-24 11:25:12 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
-import MasterHog from '../containers/MasterHog'
-import Offspring from '../db.js'
-import '../App.css'
+import React, { Component } from "react";
+import MasterHog from "../containers/MasterHog";
+import Offspring from "../db.js";
+import "../App.css";
 
 class App extends Component {
   constructor() {
-    super()
-    this.state = {
-    }
+    super();
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
-        
+        <MasterHog offspring={Offspring.offspring} />
       </div>
-    )
+    );
   }
-
 }
 
-export default App
+export default App;
